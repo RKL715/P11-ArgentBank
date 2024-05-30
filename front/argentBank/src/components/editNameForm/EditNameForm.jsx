b92ddf71fcffb20ee3b2dd5ef7e67107fce39e0e
RKL715 <remykennyl715@gmail.com>
2024-05-30 13:58:35 +0200

    ## added : * first name & last name in edit name form

--- front/argentBank/src/components/editNameForm/EditNameForm.jsx
+++ front/argentBank/src/components/editNameForm/EditNameForm.jsx
@@ -1,6 +1,8 @@
 import {useDispatch} from "react-redux";
 import {updateUserName} from "../../../redux/slice/userSlice.js";
+import {useEffect, useState} from "react";
 
+// TO FETCH USERNAME FROM SERVER
 function updateUserNameOnServer (newName) {
     return fetch('http://localhost:3001/api/v1/user/profile', {
         method: 'PUT',
@@ -12,11 +14,37 @@ function updateUserNameOnServer (newName) {
     })
 }
 
+// TO FETCH USER ID (first & last name) FROM SERVER
+function getUserIdFromServer () {
+    return fetch('http://localhost:3001/api/v1/user/profile', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${sessionStorage.getItem('token')}`
+        }
+    })
+    .then((response) => response.json())
+        .then ((data) => data.body );
+}
 
+// EDIT NAME FORM COMPONENT
 function EditNameForm ( {toggleModal} ) {
-
 const dispatch = useDispatch();
+const [firstName, setFirstName] = useState('');
+const [lastName, setLastName] = useState('');
 
+    useEffect(() => {
+        getUserIdFromServer()
+        .then((user) => {
+            setFirstName(user.firstName);
+            setLastName(user.lastName);
+        })
+        .catch((error) => {
+            console.error("Error getting user id", error);
+        })
+    }, []);
+
+// FUNCTION TO HANDLE USERNAME CHANGE
 const handleNameChange = (newName) => {
     updateUserNameOnServer(newName)
     .then (() => {
@@ -34,8 +62,12 @@ const handleNameChange = (newName) => {
           const newName = evt.target.username.value;
           handleNameChange(newName);
       }}>
-          <label htmlFor="username" className="username-label">User Name</label>
+          <label htmlFor="username" className="field-label">User Name</label>
           <input type="text" id="username" name="username" className="username-field"/>
+          <label htmlFor="firstName" className="field-label">First Name</label>
+            <input type="text" id="firstName" name="firstName" className="first-name-field" value={firstName} disabled/>
+            <label htmlFor="lastName" className="field-label">Last Name</label>
+            <input type="text" id="lastName" name="lastName" className="last-name-field" value={lastName} disabled/>
           <div className="form-button-group">
           <button type="submit" className="form-button">Save</button>
           <button type="submit" className="form-button" onClick={toggleModal}>Cancel</button>
